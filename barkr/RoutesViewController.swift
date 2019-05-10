409ef55ac014c8d52c33693d5a8eb7cfeeebcda0
lukqw <lukqw@users.noreply.github.com>
2019-05-10 02:53:54 +0200

    add app icons, routeTab including controller

---
+++ barkr/RoutesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutesViewController.swift
+//  barkr
+//
+//  Created by luk on 5/10/19.
+//  Copyright © 2019 luk. All rights reserved.
+//
+
+import UIKit
+
+class RoutesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
