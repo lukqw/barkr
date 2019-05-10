8bc566923021d0ef2c68a8ba44cdd598a90842c5
lukqw <lukqw@users.noreply.github.com>
2019-05-10 04:04:09 +0200

    add three tabs, implement test routes tab, add map to map tab, add app icon

--- barkr/RoutesViewController.swift
+++ barkr/RoutesViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class RoutesViewController: UIViewController {
+class RoutesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let favoriteRoutes:[String] = ["FavoritesTest", "FavoritesTest1"]
+    let historyRoutes:[String] = ["HistoryTest2", "HistoryTest3"]
 
+    @IBOutlet weak var routesSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var routesTableView: UITableView!
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch routesSegmentedControl.selectedSegmentIndex {
+        case 0:
+            return favoriteRoutes.count
+        case 1:
+            return historyRoutes.count
+        default:
+            return 0;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let routesCell = tableView.dequeueReusableCell(withIdentifier: "routesCell")
+        switch routesSegmentedControl.selectedSegmentIndex {
+        case 0:
+            routesCell?.textLabel!.text = favoriteRoutes[indexPath.row]
+            break
+        case 1:
+            routesCell?.textLabel!.text = historyRoutes[indexPath.row]
+            break
+        default:
+            break
+        }
+        return routesCell.unsafelyUnwrapped
+    }
+    
+    @IBAction func routesSegmentedControlPressed(_ sender: Any) {
+        routesTableView.reloadData();
+    }
+    
+    @IBAction func editRoutesPressed(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
