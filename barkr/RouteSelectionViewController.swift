0aab8ba96c2aea28d2ffa6189cc1351e7563fa83
tanyucel <tanmaster95@hotmail.com>
2019-05-16 04:24:34 +0200

    running route view mostly done, map stuff missing tho

--- barkr/RouteSelectionViewController.swift
+++ barkr/RouteSelectionViewController.swift
@@ -21,13 +21,83 @@ class RouteSelectionViewController: UIViewController, MKMapViewDelegate, UITable
                                  DogBag(coordinate: CLLocationCoordinate2D(latitude: 48.168632, longitude: 16.275215)),
                                  DogBag(coordinate: CLLocationCoordinate2D(latitude: 48.170082, longitude: 16.277615)),
                                  DogBag(coordinate: CLLocationCoordinate2D(latitude: 48.168849, longitude: 16.279870))]
-
+    var minutesValue: Int = 30
+    var kmValue: Int = 3
+    @IBOutlet weak var minOrKmButton: UIButton!
+    @IBOutlet weak var tabBar: UISegmentedControl!
+    @IBOutlet weak var minOrKmPicker: UIPickerView!
+    @IBOutlet weak var pickerContainerView: UIView!
+    @IBOutlet weak var pickerContainerDone: UIButton!
+    @IBOutlet weak var pickerContainerCancel: UIButton!
+    var unit: String = " min"
+    @IBOutlet weak var pickerContainerToolbar: UIToolbar!
+    @IBAction func tabBarSelectionChanged(_ sender: Any) {
+        updateMinOrKmButton()
+        minOrKmPicker.reloadAllComponents()
+    }
+    func updateMinOrKmButton() {
+        switch tabBar.selectedSegmentIndex {
+        case 0:
+            unit = " min"
+            minOrKmButton.setTitle(minutesValue.description + unit, for: .normal)
+        case 1:
+            unit = " km"
+            minOrKmButton.setTitle(kmValue.description + unit, for: .normal)
+        default:
+            print("default switch case shouldn't have happened")
+        }
+    }
+    @IBAction func showPicker(_ sender: Any) {
+        if tabBar.selectedSegmentIndex == 0 {
+            minOrKmPicker.selectRow(minutesValue - 1, inComponent: 0, animated: false)
+        } else {
+            minOrKmPicker.selectRow(kmValue - 1, inComponent: 0, animated: false)
+        }
+        pickerContainerView.isHidden = false
+    }
+    
+    func initializePicker() {
+        self.view.bringSubviewToFront(pickerContainerView)
+        minOrKmPicker.delegate = self
+        minOrKmPicker.dataSource = self
+        //pickerContainerView.layer.cornerRadius = 10
+        pickerContainerView.layer.borderWidth = 0.5
+        pickerContainerView.layer.borderColor = UIColor.init(white: 0.8, alpha: 1).cgColor
+        
+        minOrKmPicker.showsSelectionIndicator = true
+        pickerContainerView.isHidden = true
+        /*pickerContainerToolbar.subviews.forEach { view in
+            view.backgroundColor = UIColor.groupTableViewBackground
+            view.layer.borderWidth = 0
+        }
+        pickerContainerToolbar.layer.cornerRadius = 10
+        pickerContainerToolbar.layer.borderWidth = 0
+        pickerContainerToolbar.layer.borderColor = UIColor.groupTableViewBackground.cgColor
+        */
+        pickerContainerToolbar.subviews.forEach { view in
+            view.backgroundColor = UIColor.groupTableViewBackground
+            view.layer.borderWidth = 0
+        }
+    }
+    @IBAction func dismissVC(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func pickerValueCancelled(_ sender: Any) {
+        pickerContainerView.isHidden = true
+    }
+    @IBAction func pickerValueSelected(_ sender: Any) {
+        if tabBar.selectedSegmentIndex == 0 {
+            minutesValue = minOrKmPicker.selectedRow(inComponent: 0) + 1
+        } else {
+            kmValue = minOrKmPicker.selectedRow(inComponent: 0) + 1
+        }
+        pickerContainerView.isHidden = true
+        updateMinOrKmButton()
+    }
     @IBOutlet var routeSelectionTableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return routeArray.count
     }
-    
-
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = routeSelectionTableView.dequeueReusableCell(withIdentifier: "routeSelectionCell", for: indexPath)
         // swiftlint:disable force_cast
@@ -41,11 +111,10 @@ class RouteSelectionViewController: UIViewController, MKMapViewDelegate, UITable
 
         return cell
     }
-
     @IBOutlet var routeSelectionMap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initializePicker()
         routeSelectionMap.delegate = self
 
         let sourceLocation = routeSelectionMap.userLocation.location?.coordinate
@@ -82,7 +151,6 @@ class RouteSelectionViewController: UIViewController, MKMapViewDelegate, UITable
         directionRequest.transportType = .walking
 
         let directions = MKDirections(request: directionRequest)
-        
         directions.calculate { (response, error) in
             guard let response = response else {
                 if let error = error {
@@ -107,4 +175,24 @@ class RouteSelectionViewController: UIViewController, MKMapViewDelegate, UITable
         print("row \(indexPath.row)")
     }
 
+}
+
+extension RouteSelectionViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch tabBar.selectedSegmentIndex {
+        case 0:
+            return 180
+        case 1:
+            return 10
+        default:
+            print("default case shouldnt have happened")
+            return 100
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return (row + 1).description
+    }
 }
