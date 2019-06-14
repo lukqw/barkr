30e8715f2872362815e82d45004665599958219f
tanyucel <tanmaster95@hotmail.com>
2019-06-14 02:30:57 +0200

    #8 connected statistics view controller to realm; update route.swift so the id will be autogenerated

--- barkr/Route.swift
+++ barkr/Route.swift
@@ -21,12 +21,12 @@ class Route: Object {
     //@objc dynamic var dogBags: [DogBagRealmObject] = []
     @objc dynamic var favorite: Bool = false
     convenience init(_ time: Int, _ dogBagCount: Int, _ distance: Double) {
-        self.init(-1, time, dogBagCount,distance, false)
+        self.init(-1, time, dogBagCount, distance, false)
     }
 
     convenience init(_ id: Int, _ time: Int, _ dogBagCount: Int, _ distance: Double, _ favorite: Bool) {
         self.init()
-        self.id = id
+        self.id = incrementID()
         self.time = time
         self.dogBagCount = dogBagCount
         self.distance = distance
@@ -67,5 +67,11 @@ class Route: Object {
             dbro.longitude = dogbag.coordinate.longitude
             dogBags.append(dbro)
         }
+    }
+    func incrementID() -> Int {
+        // swiftlint:disable force_try
+        let realm = try! Realm()
+        // swiftlint:enable force_try
+        return (realm.objects(Route.self).max(ofProperty: "id") as Int? ?? 0) + 1
     }
 }
